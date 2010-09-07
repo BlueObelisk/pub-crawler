1424818061881913aa8a83b8658460dd38a15899
ned24 <ned24@localhost>
2010-09-07 12:04:01 +0100

    Added Apache 2.0 copyright messages to the top of each source file.

--- src/main/java/wwmm/pubcrawler/core/RscIssueCrawler.java
+++ src/main/java/wwmm/pubcrawler/core/RscIssueCrawler.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2010 Nick Day
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package wwmm.pubcrawler.core;
 
 import java.io.IOException;
@@ -266,7 +281,7 @@ public class RscIssueCrawler extends IssueCrawler {
 	public static void main(String[] args) throws IOException {
 		RscIssueCrawler acf = new RscIssueCrawler(RscJournal.CHEMCOMM);
 		acf.setMaxArticlesToCrawl(10);
-		List<ArticleDescription> adList = acf.getArticleDescriptions(new IssueDescription("2010", "25"));
+		List<ArticleDescription> adList = acf.getCurrentArticleDescriptions();
 		for (ArticleDescription ad : adList) {
 			System.out.println(ad.toString());
 		}
