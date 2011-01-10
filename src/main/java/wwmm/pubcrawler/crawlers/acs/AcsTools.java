90e3c063de415b69f6e7f8711987343e24ca13cb
Sam Adams <seadams@gmail.com>
2011-01-10 23:42:50 +0000

    Added more entities

--- src/main/java/wwmm/pubcrawler/crawlers/acs/AcsTools.java
+++ src/main/java/wwmm/pubcrawler/crawlers/acs/AcsTools.java
@@ -69,7 +69,18 @@ public class AcsTools {
                 if ("img".equals(childElement.getLocalName())) {
                     String src = childElement.getAttributeValue("src");
 
-                    if ("/entityImage/?text=0061,0301".equals(src)) {
+                    if ("/entityImage/?text=0065,0300".equals(src)) {
+                        element.replaceChild(child, new Text("\u00e8"));        // egrave: small a with grave
+                    }
+
+                    else if ("/entityImage/?text=0041,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u00c1"));        // Aacute: capital A with acute
+                    }
+                    else if ("/entityImage/?text=0045,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u00c9"));        // Eacute: capital E with acute
+                    }
+
+                    else if ("/entityImage/?text=0061,0301".equals(src)) {
                         element.replaceChild(child, new Text("\u00e1"));        // aacute: small a with acute
                     }
                     else if ("/entityImage/?text=0063,0301".equals(src)) {
@@ -81,9 +92,40 @@ public class AcsTools {
                     else if ("/entityImage/?text=0069,0301".equals(src)) {
                         element.replaceChild(child, new Text("\u00ed"));        // iacute: small i with acute
                     }
+                    else if ("/entityImage/?text=006e,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u0144"));        // small n with acute
+                    }
+                    else if ("/entityImage/?text=006f,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u00f3"));        // oacute: small o with acute
+                    }
+                    else if ("/entityImage/?text=0075,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u00fa"));        // uacute: small u with acute
+                    }
+                    else if ("/entityImage/?text=0079,0301".equals(src)) {
+                        element.replaceChild(child, new Text("\u00fd"));        // small y with acute
+                    }
+
+                    else if ("/entityImage/?text=006f,0302".equals(src)) {
+                        element.replaceChild(child, new Text("\u00f4"));        // small o with circumflex
+                    }
+
+                    else if ("/entityImage/?text=0061,0303".equals(src)) {
+                        element.replaceChild(child, new Text("\u00e3"));        // small a with tilde
+                    }
+                    else if ("/entityImage/?text=006e,0303".equals(src)) {
+                        element.replaceChild(child, new Text("\u00f1"));        // small n with tilde
+                    }
 
                     else if ("/entityImage/?text=0045,0305".equals(src)) {
                         element.replaceChild(child, new Text("\u0112"));        // capital E with macron
+                    }
+
+                    else if ("/entityImage/?text=0067,0306".equals(src)) {
+                        element.replaceChild(child, new Text("\u011f"));        // small g with breve
+                    }
+
+                    else if ("/entityImage/?text=007a,0307".equals(src)) {
+                        element.replaceChild(child, new Text("\u017c"));        // small z with dot above ??
                     }
 
                     else if ("/entityImage/?text=0061,0308".equals(src)) {
@@ -92,17 +134,54 @@ public class AcsTools {
                     else if ("/entityImage/?text=0065,0308".equals(src)) {
                         element.replaceChild(child, new Text("\u00eb"));        // euml: small e with diaeresis
                     }
+                    else if ("/entityImage/?text=0069,0308".equals(src)) {
+                        element.replaceChild(child, new Text("\u00ef"));        // iuml: small i with diaeresis
+                    }
                     else if ("/entityImage/?text=006f,0308".equals(src)) {
                         element.replaceChild(child, new Text("\u00f6"));        // ouml: small o with diaeresis
+                    }
+                    else if ("/entityImage/?text=0075,0308".equals(src)) {
+                        element.replaceChild(child, new Text("\u00fc"));        // uuml: small u with diaeresis
+                    }
+
+                    else if ("/entityImage/?text=0061,030a".equals(src)) {
+                        element.replaceChild(child, new Text("\u00e5"));        // small a with ring above
+                    }
+
+                    else if ("/entityImage/?text=006f,030b".equals(src)) {
+                        element.replaceChild(child, new Text("\u0151"));        // small o with double acute 
+                    }
+
+                    // http://pubs.acs.org/doi/suppl/10.1021/ic902279k
+                    else if ("/entityImage/?text=0043,030c".equals(src)) {
+                        element.replaceChild(child, new Text("\u010c"));        // capital C with caron
+                    }
+                    else if ("/entityImage/?text=0053,030c".equals(src)) {
+                        element.replaceChild(child, new Text("\u0160"));        // capital S with caron
                     }
 
                     else if ("/entityImage/?text=0063,030c".equals(src)) {
                         element.replaceChild(child, new Text("\u010d"));        // small c with caron
                     }
+                    else if ("/entityImage/?text=0073,030c".equals(src)) {
+                        element.replaceChild(child, new Text("\u0161"));        // small s with caron
+                    }
 
                     else if ("/entityImage/?text=0063,0327".equals(src)) {
                         element.replaceChild(child, new Text("\u00e7"));        // ccedil: small c with cedilla
                     }
+                    else if ("/entityImage/?text=0065,0327".equals(src)) {
+                        element.replaceChild(child, new Text("\u0229"));        // small e with cedilla
+                    }
+                    else if ("/entityImage/?text=0074,0327".equals(src)) {
+                        element.replaceChild(child, new Text("\u0163"));        // small t with cedilla ??
+                    }
+
+                    // http://pubs.acs.org/doi/suppl/10.1021/ic902279k
+                    else if ("/entityImage/?text=006f,0337".equals(src)) {
+                        element.replaceChild(child, new Text("\u00f8"));        // small o with stroke ??
+                    }
+
 
                     else {
                         System.out.println(src);
