104756533d21f7f94f00da1c422d034ad1c47a89
Sam Adams <seadams@gmail.com>
2011-01-30 11:06:13 +0000

    Added Acta entity mappings

--- src/main/java/wwmm/pubcrawler/Unicode.java
+++ src/main/java/wwmm/pubcrawler/Unicode.java
@@ -21,6 +21,8 @@ package wwmm.pubcrawler;
  */
 public class Unicode {
 
+    public static final char ELIPSIS        = '\u2026';
+
     public static final char GREEK_ALPHA_LC         = '\u03b1';
     public static final char GREEK_BETA_LC          = '\u03b2';
     public static final char GREEK_GAMMA_LC         = '\u03b3';
@@ -46,6 +48,32 @@ public class Unicode {
     public static final char GREEK_CHI_LC           = '\u03c7';
     public static final char GREEK_PSI_LC           = '\u03c8';
     public static final char GREEK_OMEGA_LC         = '\u03c9';
+
+    public static final char GREEK_ALPHA_UC         = '\u0391';
+    public static final char GREEK_BETA_UC          = '\u0392';
+    public static final char GREEK_GAMMA_UC         = '\u0393';
+    public static final char GREEK_DELTA_UC         = '\u0394';
+    public static final char GREEK_EPSION_UC        = '\u0395';
+    public static final char GREEK_ZETA_UC          = '\u0396';
+    public static final char GREEK_ETA_UC           = '\u0397';
+    public static final char GREEK_THETA_UC         = '\u0398';
+    public static final char GREEK_IOTA_UC          = '\u0399';
+    public static final char GREEK_KAPPA_UC         = '\u039a';
+    public static final char GREEK_LAMBDA_UC        = '\u039b';
+    public static final char GREEK_MU_UC            = '\u039c';
+    public static final char GREEK_NU_UC            = '\u039d';
+    public static final char GREEK_XI_UC            = '\u039e';
+    public static final char GREEK_OMNICRON_UC      = '\u039f';
+    public static final char GREEK_PI_UC            = '\u03a0';
+    public static final char GREEK_RHO_UC           = '\u03a1';
+    //
+    public static final char GREEK_SIGMA_UC         = '\u03a3';
+    public static final char GREEK_TAU_UC           = '\u03a4';
+    public static final char GREEK_UPSILON_UC       = '\u03a5';
+    public static final char GREEK_PHI_UC           = '\u03a6';
+    public static final char GREEK_CHI_UC           = '\u03a7';
+    public static final char GREEK_PSI_UC           = '\u03a8';
+    public static final char GREEK_OMEGA_UC         = '\u03a9';
 
     public static final Character ASYMPTOTICALLY_EQUAL_TO   = '\u2243';
     public static final Character TILDE_OPERATOR            = '\u223c';
