8b293fe435dcb6698bc400dd637c4f9693b355e8
Sam Adams <seadams@gmail.com>
2011-01-30 11:59:39 +0000

    Added Acs entity mappings

--- src/main/java/wwmm/pubcrawler/crawlers/acs/AcsTools.java
+++ src/main/java/wwmm/pubcrawler/crawlers/acs/AcsTools.java
@@ -18,10 +18,14 @@ package wwmm.pubcrawler.crawlers.acs;
 
 import nu.xom.*;
 import org.apache.log4j.Logger;
+import wwmm.pubcrawler.Unicode;
 import wwmm.pubcrawler.utils.XPathUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Sam Adams
@@ -29,6 +33,100 @@ import java.io.IOException;
 public class AcsTools {
 
     private static final Logger LOG = Logger.getLogger(AcsTools.class);
+
+    private static final Map<String,Character> ENTITY_MAP;
+
+    static {
+        Map<String,Character> map = new HashMap<String, Character>();
+
+        // 0300 = grave
+
+        map.put("/entityImage/?text=0041,0300", Unicode.Latin1_Supplement.  Latin_Capital_Letter_A_With_Grave);
+
+        map.put("/entityImage/?text=0061,0300", Unicode.Latin1_Supplement.  Latin_Small_Letter_A_With_Grave);
+        map.put("/entityImage/?text=0065,0300", Unicode.Latin1_Supplement.  Latin_Small_Letter_E_With_Grave);
+        map.put("/entityImage/?text=0069,0300", Unicode.Latin1_Supplement.  Latin_Small_Letter_I_With_Grave);
+        map.put("/entityImage/?text=006f,0300", Unicode.Latin1_Supplement.  Latin_Small_Letter_O_With_Grave);
+        map.put("/entityImage/?text=0075,0300", Unicode.Latin1_Supplement.  Latin_Small_Letter_U_With_Grave);
+
+
+        // 0301 = acute
+
+        map.put("/entityImage/?text=0041,0301", Unicode.Latin1_Supplement.  Latin_Capital_Letter_A_With_Acute);
+        map.put("/entityImage/?text=0043,0301", Unicode.LatinExtendedA.     Latin_Capital_Letter_C_With_Acute);
+        map.put("/entityImage/?text=0045,0301", Unicode.Latin1_Supplement.  Latin_Capital_Letter_E_With_Acute);
+        map.put("/entityImage/?text=0049,0301", Unicode.Latin1_Supplement.  Latin_Capital_Letter_I_With_Acute);
+        map.put("/entityImage/?text=0053,0301", Unicode.LatinExtendedA.     Latin_Capital_Letter_S_With_Acute);
+
+        map.put("/entityImage/?text=0061,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_A_With_Acute);
+        map.put("/entityImage/?text=0063,0301", Unicode.LatinExtendedA.     Latin_Small_Letter_C_With_Acute);
+        map.put("/entityImage/?text=0065,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_E_With_Acute);
+        map.put("/entityImage/?text=0069,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_I_With_Acute);
+        map.put("/entityImage/?text=006e,0301", Unicode.LatinExtendedA.     Latin_Small_Letter_N_With_Acute);
+        map.put("/entityImage/?text=006f,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_O_With_Acute);
+        map.put("/entityImage/?text=0073,0301", Unicode.LatinExtendedA.     Latin_Small_Letter_S_With_Acute);
+        map.put("/entityImage/?text=0075,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_U_With_Acute);
+        map.put("/entityImage/?text=0079,0301", Unicode.Latin1_Supplement.  Latin_Small_Letter_Y_With_Acute);
+        map.put("/entityImage/?text=007a,0301", Unicode.LatinExtendedA.     Latin_Small_Letter_Z_With_Acute);
+
+
+        // 0302 = circumflex
+
+        map.put("/entityImage/?text=0041,0302", Unicode.Latin1_Supplement.  Latin_Capital_Letter_A_With_Circumflex);
+//                    map.put("/entityImage/?text=004c,0302", Unicode.LatinExtendedA.     Latin_Capital_Letter_L_With_Circumflex);
+
+        map.put("/entityImage/?text=0061,0302", Unicode.Latin1_Supplement.  Latin_Small_Letter_A_With_Circumflex);
+        map.put("/entityImage/?text=0065,0302", Unicode.Latin1_Supplement.  Latin_Small_Letter_E_With_Circumflex);
+        map.put("/entityImage/?text=0069,0302", Unicode.Latin1_Supplement.  Latin_Small_Letter_I_With_Circumflex);
+        map.put("/entityImage/?text=006f,0302", Unicode.Latin1_Supplement.  Latin_Small_Letter_O_With_Circumflex);
+
+
+        // 0303 = tilde
+
+        map.put("/entityImage/?text=0061,0303", Unicode.Latin1_Supplement.  Latin_Small_Letter_A_With_Tilde);
+        map.put("/entityImage/?text=006e,0303", Unicode.Latin1_Supplement.  Latin_Small_Letter_N_With_Tilde);
+//                    map.put("/entityImage/?text=0073,0303", Unicode.LatinExtendedA.     Latin_Small_Letter_S_With_Tilde);
+
+
+        // 0305 = macron
+
+        map.put("/entityImage/?text=0045,0305", Unicode.LatinExtendedA.     Latin_Capital_Letter_E_With_Macron);
+        map.put("/entityImage/?text=004f,0305", Unicode.LatinExtendedA.     Latin_Capital_Letter_O_With_Macron);
+
+        map.put("/entityImage/?text=0065,0305", Unicode.LatinExtendedA.     Latin_Small_Letter_E_With_Macron);
+
+
+        // 0306 = breve
+
+        map.put("/entityImage/?text=0061,0306", Unicode.LatinExtendedA.     Latin_Small_Letter_A_With_Breve);
+        map.put("/entityImage/?text=0065,0306", Unicode.LatinExtendedA.     Latin_Small_Letter_E_With_Breve);
+        map.put("/entityImage/?text=0067,0306", Unicode.LatinExtendedA.     Latin_Small_Letter_G_With_Breve);
+
+
+        // 0307 = dot above
+
+        map.put("/entityImage/?text=0049,0307", Unicode.LatinExtendedA.     Latin_Capital_Letter_I_With_Dot_Above);
+        map.put("/entityImage/?text=005a,0307", Unicode.LatinExtendedA.     Latin_Capital_Letter_Z_With_Dot_Above);
+
+        map.put("/entityImage/?text=0065,0307", Unicode.LatinExtendedA.     Latin_Small_Letter_E_With_Dot_Above);
+//                    map.put("/entityImage/?text=0075,0307", Unicode.LatinExtendedA.     Latin_Small_Letter_U_With_Dot_Above);
+        map.put("/entityImage/?text=007a,0307", Unicode.LatinExtendedA.     Latin_Small_Letter_Z_With_Dot_Above);
+
+
+        // 0308 = diaeresis
+
+        map.put("/entityImage/?text=0041,0308", Unicode.Latin1_Supplement.  Latin_Capital_Letter_A_With_Diaeresis);
+        map.put("/entityImage/?text=004f,0308", Unicode.Latin1_Supplement.  Latin_Capital_Letter_O_With_Diaeresis);
+        map.put("/entityImage/?text=0055,0308", Unicode.Latin1_Supplement.  Latin_Capital_Letter_U_With_Diaeresis);
+
+        map.put("/entityImage/?text=0061,0308", Unicode.Latin1_Supplement.  Latin_Small_Letter_A_With_Diaeresis);
+        map.put("/entityImage/?text=0065,0308", Unicode.Latin1_Supplement.  Latin_Small_Letter_E_With_Diaeresis);
+        map.put("/entityImage/?text=0069,0308", Unicode.Latin1_Supplement.  Latin_Small_Letter_I_With_Diaeresis);
+        map.put("/entityImage/?text=006f,0308", Unicode.Latin1_Supplement.  Latin_Small_Letter_O_With_Diaeresis);
+        map.put("/entityImage/?text=0075,0308", Unicode.Latin1_Supplement.  Latin_Small_Letter_U_With_Diaeresis);
+
+        ENTITY_MAP = Collections.unmodifiableMap(map);
+    }
 
     public static Element getTitleHtml(Node node) {
         Element element = (Element) XPathUtils.getNode(node, ".//x:h1[@class='articleTitle']");
@@ -71,79 +169,9 @@ public class AcsTools {
                 if ("img".equals(childElement.getLocalName())) {
                     String src = childElement.getAttributeValue("src");
 
-                    if ("/entityImage/?text=0065,0300".equals(src)) {
-                        element.replaceChild(child, new Text("\u00e8"));        // egrave: small a with grave
-                    }
-
-                    else if ("/entityImage/?text=0041,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00c1"));        // Aacute: capital A with acute
-                    }
-                    else if ("/entityImage/?text=0045,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00c9"));        // Eacute: capital E with acute
-                    }
-
-                    else if ("/entityImage/?text=0061,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00e1"));        // aacute: small a with acute
-                    }
-                    else if ("/entityImage/?text=0063,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u0107"));        // small c with acute
-                    }
-                    else if ("/entityImage/?text=0065,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00e9"));        // eacute: small e with acute
-                    }
-                    else if ("/entityImage/?text=0069,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00ed"));        // iacute: small i with acute
-                    }
-                    else if ("/entityImage/?text=006e,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u0144"));        // small n with acute
-                    }
-                    else if ("/entityImage/?text=006f,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00f3"));        // oacute: small o with acute
-                    }
-                    else if ("/entityImage/?text=0075,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00fa"));        // uacute: small u with acute
-                    }
-                    else if ("/entityImage/?text=0079,0301".equals(src)) {
-                        element.replaceChild(child, new Text("\u00fd"));        // small y with acute
-                    }
-
-                    else if ("/entityImage/?text=006f,0302".equals(src)) {
-                        element.replaceChild(child, new Text("\u00f4"));        // small o with circumflex
-                    }
-
-                    else if ("/entityImage/?text=0061,0303".equals(src)) {
-                        element.replaceChild(child, new Text("\u00e3"));        // small a with tilde
-                    }
-                    else if ("/entityImage/?text=006e,0303".equals(src)) {
-                        element.replaceChild(child, new Text("\u00f1"));        // small n with tilde
-                    }
-
-                    else if ("/entityImage/?text=0045,0305".equals(src)) {
-                        element.replaceChild(child, new Text("\u0112"));        // capital E with macron
-                    }
-
-                    else if ("/entityImage/?text=0067,0306".equals(src)) {
-                        element.replaceChild(child, new Text("\u011f"));        // small g with breve
-                    }
-
-                    else if ("/entityImage/?text=007a,0307".equals(src)) {
-                        element.replaceChild(child, new Text("\u017c"));        // small z with dot above ??
-                    }
-
-                    else if ("/entityImage/?text=0061,0308".equals(src)) {
-                        element.replaceChild(child, new Text("\u00e4"));        // auml: small a with diaeresis
-                    }
-                    else if ("/entityImage/?text=0065,0308".equals(src)) {
-                        element.replaceChild(child, new Text("\u00eb"));        // euml: small e with diaeresis
-                    }
-                    else if ("/entityImage/?text=0069,0308".equals(src)) {
-                        element.replaceChild(child, new Text("\u00ef"));        // iuml: small i with diaeresis
-                    }
-                    else if ("/entityImage/?text=006f,0308".equals(src)) {
-                        element.replaceChild(child, new Text("\u00f6"));        // ouml: small o with diaeresis
-                    }
-                    else if ("/entityImage/?text=0075,0308".equals(src)) {
-                        element.replaceChild(child, new Text("\u00fc"));        // uuml: small u with diaeresis
+                    Character c = ENTITY_MAP.get(src);
+                    if (c != null) {
+                        element.replaceChild(child, new Text(c.toString()));
                     }
 
                     else if ("/entityImage/?text=0061,030a".equals(src)) {
