c3c8eeebda17f492c3bac4d8d18a8603e0b543c9
Sam Adams <seadams@gmail.com>
2011-02-05 09:36:47 +0000

    Recorded journal index URIs for new publishers

---
+++ src/main/java/wwmm/pubcrawler/crawlers/bmc/BmcPublicationCrawler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010-2011 Nick Day, Sam Adams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wwmm.pubcrawler.crawlers.bmc;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+import java.net.URI;
+
+/**
+ * @author Sam Adams
+ */
+public class BmcPublicationCrawler {
+
+    private static final URI OPML_URI = URI.create("http://www.biomedcentral.com/info/journals/biomedcentraljournallist.opml");
+
+}
