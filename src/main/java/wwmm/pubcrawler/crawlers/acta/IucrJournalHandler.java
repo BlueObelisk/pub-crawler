71235176e30fc33b9c0d1c97afe1e201d3646d2f
Sam Adams <seadams@gmail.com>
2012-04-16 21:45:14 +0100

    Fixes to IUCr crawlers

---
+++ src/main/java/wwmm/pubcrawler/crawlers/acta/IucrJournalHandler.java
@@ -0,0 +1,49 @@
+package wwmm.pubcrawler.crawlers.acta;
+
+import org.joda.time.Duration;
+import wwmm.pubcrawler.crawler.CrawlRunner;
+import wwmm.pubcrawler.crawler.CrawlTask;
+import wwmm.pubcrawler.crawler.CrawlTaskBuilder;
+import wwmm.pubcrawler.crawler.TaskQueue;
+import wwmm.pubcrawler.crawlers.JournalHandler;
+import wwmm.pubcrawler.crawlers.acta.tasks.IucrIssueListCrawlerTask;
+import wwmm.pubcrawler.model.Journal;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sam Adams
+ */
+public class IucrJournalHandler implements JournalHandler {
+
+    private final TaskQueue taskQueue;
+
+    @Inject
+    public IucrJournalHandler(final TaskQueue taskQueue) {
+        this.taskQueue = taskQueue;
+    }
+
+    @Override
+    public void handleJournal(final Journal journal) {
+        CrawlTask task = createTask(journal);
+        taskQueue.queueTask(task);
+    }
+
+    private CrawlTask createTask(final Journal journal) {
+        final Map<String, String> map = new HashMap<String, String>();
+        map.put("url", "http://journals.iucr.org/" + journal.getAbbreviation() +"/contents/backissuesbdy.html");
+        map.put("fileId", "issuelist.html");
+        map.put("publisher", "iucr");
+        map.put("journal", journal.getAbbreviation());
+
+        return new CrawlTaskBuilder()
+            .ofType(IucrIssueListCrawlerTask.class)
+            .withMaxAge(Duration.standardDays(1))
+            .withId("iucr:issue-list:" + journal.getAbbreviation())
+            .withData(map)
+            .build();
+    }
+
+}
--- src/main/java/wwmm/pubcrawler/crawlers/acta/tasks/IucrIssueListCrawlerTask.java
+++ src/main/java/wwmm/pubcrawler/crawlers/acta/tasks/IucrIssueListCrawlerTask.java
@@ -1,17 +1,51 @@
 package wwmm.pubcrawler.crawlers.acta.tasks;
 
+import nu.xom.Document;
+import uk.ac.cam.ch.wwmm.httpcrawler.CrawlerResponse;
+import wwmm.pubcrawler.controller.Fetcher;
+import wwmm.pubcrawler.controller.URITask;
 import wwmm.pubcrawler.crawler.CrawlRunner;
 import wwmm.pubcrawler.crawler.TaskData;
 import wwmm.pubcrawler.crawler.common.HtmlCrawler;
+import wwmm.pubcrawler.crawlers.BasicHttpCrawlTask;
+import wwmm.pubcrawler.crawlers.IssueHandler;
+import wwmm.pubcrawler.crawlers.IssueTocParser;
+import wwmm.pubcrawler.crawlers.acta.parsers.IucrIssueListParser;
+import wwmm.pubcrawler.model.Article;
+import wwmm.pubcrawler.model.Issue;
+import wwmm.pubcrawler.model.id.JournalId;
+import wwmm.pubcrawler.model.id.PublisherId;
+import wwmm.pubcrawler.utils.HtmlUtils;
+
+import javax.inject.Inject;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @author Sam Adams
  */
-public class IucrIssueListCrawlerTask extends HtmlCrawler implements CrawlRunner {
+public class IucrIssueListCrawlerTask extends BasicHttpCrawlTask {
+
+    private final IssueHandler issueHandler;
+
+    @Inject
+    public IucrIssueListCrawlerTask(final Fetcher<URITask, CrawlerResponse> fetcher, final IssueHandler issueHandler) {
+        super(fetcher);
+        this.issueHandler = issueHandler;
+    }
 
     @Override
-    public void run(final String id, final TaskData data) throws Exception {
-        //To change body of implemented methods use File | Settings | File Templates.
+    protected void handleResponse(final String id, final TaskData data, final CrawlerResponse response) throws Exception {
+        final Document html = HtmlUtils.readHtmlDocument(response);
+        final URI url = URI.create(data.getString("url"));
+
+        final PublisherId publisherId = new PublisherId(data.getString("publisher"));
+        final JournalId journalId = new JournalId(publisherId, data.getString("journal"));
+
+        final IucrIssueListParser parser = new IucrIssueListParser(html, url, journalId);
+        for (final Issue issue : parser.findIssues()) {
+            issueHandler.handleIssueLink(issue);
+        }
     }
 
 }
