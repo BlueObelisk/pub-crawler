f0ee357d5e04662118ade5dab3155d72483226d3
Sam Adams <seadams@gmail.com>
2012-04-19 07:23:17 +0100

    Modularised project

---
+++ publishers/rsc/src/main/java/wwmm/pubcrawler/crawlers/rsc/RscIssueTocCrawlTaskFactory.java
@@ -0,0 +1,20 @@
+package wwmm.pubcrawler.crawlers.rsc;
+
+import wwmm.pubcrawler.crawler.CrawlRunner;
+import wwmm.pubcrawler.crawlers.AbstractIssueTocCrawlTaskFactory;
+import wwmm.pubcrawler.crawlers.rsc.tasks.RscIssueTocCrawlTask;
+
+import javax.inject.Singleton;
+
+/**
+ * @author Sam Adams
+ */
+@Singleton
+public class RscIssueTocCrawlTaskFactory extends AbstractIssueTocCrawlTaskFactory {
+
+    @Override
+    protected Class<? extends CrawlRunner> getCrawlerType() {
+        return RscIssueTocCrawlTask.class;
+    }
+
+}
