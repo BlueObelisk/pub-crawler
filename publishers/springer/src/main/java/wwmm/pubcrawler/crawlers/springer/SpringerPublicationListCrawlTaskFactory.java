f4badaaa89f31c60297f2382251ef7f166a27146
Sam Adams <seadams@gmail.com>
2012-04-19 07:23:17 +0100

    Modularised project

---
+++ publishers/springer/src/main/java/wwmm/pubcrawler/crawlers/springer/SpringerPublicationListCrawlTaskFactory.java
@@ -0,0 +1,33 @@
+package wwmm.pubcrawler.crawlers.springer;
+
+import org.joda.time.Duration;
+import wwmm.pubcrawler.crawlers.springer.tasks.SpringerPublicationListCrawlTask;
+import wwmm.pubcrawler.crawler.CrawlTask;
+import wwmm.pubcrawler.crawler.CrawlTaskBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sam Adams
+ */
+public class SpringerPublicationListCrawlTaskFactory {
+
+    public CrawlTask createIssueListCrawlTask(final URI url, final String key, final int page) {
+        // Fetch pages
+        final Map<String,String> data = new HashMap<String, String>();
+        data.put("url", url.toString());
+        data.put("fileId", "journals_" + key + "_" + page + ".html");
+        data.put("key", key);
+        data.put("page", Integer.toString(page));
+
+        return new CrawlTaskBuilder()
+            .ofType(SpringerPublicationListCrawlTask.class)
+            .withData(data)
+            .withMaxAge(Duration.standardDays(1))
+            .withId("springer:journal-list/" + key + "/" + page)
+            .build();
+    }
+
+}
