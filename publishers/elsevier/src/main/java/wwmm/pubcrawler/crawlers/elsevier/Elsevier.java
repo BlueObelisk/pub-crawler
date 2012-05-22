a5a6e5493edead9ec7b73a2b6596b94399b00777
Sam Adams <seadams@gmail.com>
2012-05-22 21:41:58 +0100

    fixed file missed in refactoring

--- publishers/elsevier/src/main/java/wwmm/pubcrawler/crawlers/elsevier/Elsevier.java
+++ publishers/elsevier/src/main/java/wwmm/pubcrawler/crawlers/elsevier/Elsevier.java
@@ -1,39 +1,16 @@
 package wwmm.pubcrawler.crawlers.elsevier;
 
-import org.joda.time.Duration;
-import wwmm.pubcrawler.crawlers.HttpCrawlTask;
-import wwmm.pubcrawler.crawlers.elsevier.tasks.ElsevierIssueTocCrawlTask;
 import wwmm.pubcrawler.model.id.PublisherId;
-import wwmm.pubcrawler.crawler.CrawlTask;
-import wwmm.pubcrawler.crawler.CrawlTaskBuilder;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-
-import static wwmm.pubcrawler.crawlers.HttpCrawlTask.FILE_ID;
-import static wwmm.pubcrawler.crawlers.HttpCrawlTask.URL;
 
 /**
  * @author Sam Adams
  */
 public class Elsevier {
-    
+
     public static final URI OPML_URL = URI.create("http://feeds.sciencedirect.com/opml.xml");
 
     public static final PublisherId PUBLISHER_ID = new PublisherId("elsevier");
 
-    public static CrawlTask createIssueTocTask(final URI url, final String journal, final String id) {
-        final Map<String, String> map = new HashMap<String, String>();
-        map.put(URL, url.toString());
-        map.put(FILE_ID, "toc.html");
-        map.put("journal", journal);
-
-        return new CrawlTaskBuilder()
-            .ofType(ElsevierIssueTocCrawlTask.class)
-            .withInterval(Duration.standardDays(1))
-            .withId("elsevier:issue-toc:" + journal + "/" + id)
-            .withData(map)
-            .build();
-    }
 }
